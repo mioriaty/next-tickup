6c800e8d187be1e5ba0074e1e07145e4c548ee7e
duongto <mioriaty@gmail.com>
2024-08-13 08:15:10 +0700

    settings dependencies

---
+++ src/app/contracts/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function ContractsPage() {
+  return (
+    <div>
+      <Link href={'/'}>Heheh</Link>
+      <h1>Contracts</h1>
+    </div>
+  );
+}
